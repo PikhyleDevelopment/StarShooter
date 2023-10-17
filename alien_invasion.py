34a4c3bcba5fa637d62c8e631e322e7cdccbfb10
Erik Mason <erik@pikhyle.net>
2023-10-16 23:29:08 -0400

    Left/right movement, screen bounds checking, and full screen mode.

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,9 +14,14 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+        if self.settings.full_screen:
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.settings.screen_width = self.screen.get_rect().width
+            self.settings.screen_height = self.screen.get_rect().height
+        else:
+            self.screen = pygame.display.set_mode(
+                (self.settings.screen_width, self.settings.screen_height)
+            )
         pygame.display.set_caption("Alien Invasion")
         self.bg_image = pygame.image.load(self.settings.bg_image)
         self.bg_image = pygame.transform.scale(
@@ -33,17 +38,45 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
+            self._check_events()
+            self.ship.update()
             # Redraw the screen during each pass through the loop
-            # self.screen.fill(self.settings.bg_color)
-            self.screen.blit(self.bg_image, (0, 0))
-            self.ship.blitme()
+            self._update_screen()
 
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to key presses"""
+        if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+            # Move the ship to the right
+            self.ship.moving_right = True
+        if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+            self.ship.moving_left = True
+        if event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the screen and flip to new screen"""
+        # self.screen.fill(self.settings.bg_color)
+        self.screen.blit(self.bg_image, (0, 0))
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -4,8 +4,12 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings"""
         # Screen settings
+        self.full_screen = False
         self.screen_width = 1920
         self.screen_height = 1080
         self.bg_color = (32, 32, 32)
-        self.ship_image = 'images/SpaceShooterRedux/PNG/playerShip1_blue.png'
         self.bg_image = 'images/SpaceShooterRedux/Backgrounds/darkPurple.png'
+
+        # Ship settings
+        self.ship_image = 'images/SpaceShooterRedux/PNG/playerShip1_blue.png'
+        self.ship_speed = 1.5
