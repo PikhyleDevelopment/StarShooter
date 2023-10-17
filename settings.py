a2a51d64c6b3984baffa3be71f19df19002c8d7b
Erik Mason <erik@pikhyle.net>
2023-10-16 22:46:01 -0400

    Initial Commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        # Screen settings
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.bg_color = (32, 32, 32)
+        self.ship_image = 'images/SpaceShooterRedux/PNG/playerShip1_blue.png'
+        self.bg_image = 'images/SpaceShooterRedux/Backgrounds/darkPurple.png'
