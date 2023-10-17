d1bf35ad5d738a4578f4cc4acbf7b9c2839ac0a3
Erik Mason <erik@pikhyle.net>
2023-10-17 09:04:39 -0400

    Ship can now fire bullets, only 5 on screen at a time. Bullets are deleted once off the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 
+from bullet import Bullet
 from settings import Settings
 from ship import Ship
 
@@ -13,15 +14,18 @@ class AlienInvasion:
         """Initializes the game, and create game resources"""
         pygame.init()
         self.settings = Settings()
+        self.bullets = pygame.sprite.Group()
 
         if self.settings.full_screen:
             self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
             self.settings.screen_width = self.screen.get_rect().width
             self.settings.screen_height = self.screen.get_rect().height
+            self.settings.ship_speed = 2
         else:
             self.screen = pygame.display.set_mode(
                 (self.settings.screen_width, self.settings.screen_height)
             )
+        self.ship = Ship(self)
         pygame.display.set_caption("Alien Invasion")
         self.bg_image = pygame.image.load(self.settings.bg_image)
         self.bg_image = pygame.transform.scale(
@@ -32,14 +36,15 @@ class AlienInvasion:
             )
         )
 
-        self.ship = Ship(self)
-
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             # Watch for keyboard and mouse events
             self._check_events()
+            # Update the ship
             self.ship.update()
+            # Update the bullets
+            self._update_bullets()
             # Redraw the screen during each pass through the loop
             self._update_screen()
 
@@ -60,6 +65,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         if event.key == pygame.K_LEFT or event.key == pygame.K_a:
             self.ship.moving_left = True
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()
         if event.key == pygame.K_q:
             sys.exit()
 
@@ -69,14 +76,28 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT or event.key == pygame.K_a:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullet_inventory:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on the screen and flip to new screen"""
         # self.screen.fill(self.settings.bg_color)
         self.screen.blit(self.bg_image, (0, 0))
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
+
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -5,6 +5,8 @@ class Settings:
         """Initialize the game's settings"""
         # Screen settings
         self.full_screen = False
+        # Screen width and height if self.full_screen is False.
+        # Otherwise, these settings are ignored. See alien_invasion.__init__()
         self.screen_width = 1920
         self.screen_height = 1080
         self.bg_color = (32, 32, 32)
@@ -13,3 +15,10 @@ class Settings:
         # Ship settings
         self.ship_image = 'images/SpaceShooterRedux/PNG/playerShip1_blue.png'
         self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (0, 255, 0)
+        self.bullet_inventory = 5
